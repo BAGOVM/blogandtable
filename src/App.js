ac0f8d48af5d0062967625b28983fff15cbdfdcd
bagov <bagovname@gmail.com>
2021-11-21 15:54:33 +0300

    commit 2

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import './App.css';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
+import { PublicRoute } from './components/PublicRoute/PublicRoute';
+import { BlogPage } from './containers/BlogPage/BlogPage';
+import { LoginPage } from './containers/LoginPage/LoginPage';
+import { NoMatch } from './containers/NoMatch/NoMatch';
+
+export function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('isLoggedIn') === 'true'
+  );
+  const [userName, setUserName] = useState(localStorage.getItem('userName'));
+
+  return (
+    <Router>
+      <div className='App'>
+        <main>
+          <Switch>
+            <Route exact path='/' render={() => <Redirect to='/blog' />} />
+
+            <Route exact path='/login'>
+              <LoginPage
+                setIsLoggedIn={setIsLoggedIn}
+                setUserName={setUserName}
+              />
+            </Route>
+
+            <Route exact path='/blog'>
+              <BlogPage
+                userName={userName}
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            </Route>
+
+            <Route exact path='/404'>
+              <NoMatch />
+            </Route>
+
+            <Route
+              path='*'
+              render={({ location }) => {
+                return (
+                  <Redirect
+                    to={{
+                      pathname: '/404',
+                      from: location,
+                    }}
+                  />
+                );
+              }}
+            />
+          </Switch>
+        </main>
+
+        <Footer year={new Date().getFullYear()} />
+      </div>
+    </Router>
+  );
+}
---
+++ src/containers/LoginPage/LoginPage.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+import "./LoginPage.css";
+
+export const LoginPage = ({
+  setIsLoggedIn,
+  setUserName
+}) => {
+
+  const history = useHistory()
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginChange = (e) => {
+    setLogin(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+
+    if (login === 'admin' && password === '123456') {
+      localStorage.setItem('isLoggedIn', true);
+      localStorage.setItem('userName', login);
+      setUserName(login);
+      setIsLoggedIn(true);
+      history.push('/');
+    }
+    else {
+      alert('Введите правильный логин или пароль!');
+      return false
+    }
+  }
+
+  return (
+    <h1>
+      <form className="loginForm" onSubmit={handleLogIn}>
+        <h2>Авторизация</h2>
+        <div>
+          <input
+            className="loginFormInput"
+            type="text"
+            placeholder="Логин"
+            onChange={handleLoginChange}
+            value={login}
+            required
+          />
+        </div>
+        <div>
+          <input
+            className="loginFormInput"
+            type="password"
+            placeholder="Пароль"
+            onChange={handlePasswordChange}
+            value={password}
+            required
+          />
+        </div>
+        <div>
+          <button className="blackBtn" type="submit">
+            Войти
+          </button>
+        </div>
+      </form>
+    </h1>
+  );
+};
